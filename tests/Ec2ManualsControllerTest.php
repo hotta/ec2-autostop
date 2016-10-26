fc09a292df0205539469f68f3d16c7a16c04ee61
Michihide Hotta <hotta@net-newbie.com>
2016-10-26 14:09:31 +0900

    all test passwd so far

--- tests/Ec2ManualsControllerTest.php
+++ tests/Ec2ManualsControllerTest.php
@@ -1,4 +1,9 @@
 <?php
+//
+// 注意！以下のケースでは意図しない状態遷移が発生してテストに失敗する：
+//  ・queue:listen や queue:work が動いている
+//  ・デフォルトの phpunit.xml 
+//    - QUEUE_DRIVER=sync になっており、ジョブのキューイングが行われない。
 
 use App\Console\Commands\Ec2AutostopCommand;
 use Symfony\Component\Console\Application;
@@ -33,11 +38,12 @@ class Ec2ManualsControllerTest extends Ec2TestCase
    */
   public function testEc2ManualsInit()
   {
-    $one_hour_after = date('H:i:0', time() + 3600);  //  現在時刻の１時間後
+    $one_hour_after = date('H:i:00', time() + 3600); //  現在時刻の１時間後
     $this->fake->changeState(self::INSTANCE_ID, 'running');
     $this->fake->changeTerminable(self::INSTANCE_ID, true);
     $this->fake->changeStopAt(self::INSTANCE_ID, $one_hour_after);
-    $this->visit('/')             //  ここにアクセスして
+    $this->visit('/')             //  ここにアクセスすると
+         ->see($one_hour_after)   //  これが表示されており、
          ->see(self::NICKNAME)    //  これが表示されることを確認
          ->see('動作中')
          ->see('停止')
@@ -46,26 +52,58 @@ class Ec2ManualsControllerTest extends Ec2TestCase
   }
 
   /**
-   * 「停止」ボタンを押すとインスタンスを停止する
-   *
-   *  注意！crontab で schedule:run が動いていると、バックグラウンドで
-   *  状態遷移が起こってテストに失敗するので、コメントアウトしてから行うこと。
+   * 「停止」ボタンを押すと「停止処理中」に移行する
    *
    */
   public function testEc2ManualsPressStop()
   {
-    $one_hour_after = date('H:i:0', time() + 3600);  //  現在時刻の１時間後
+    $one_hour_after = date('H:i:00', time() + 3600); //  現在時刻の１時間後
     $this->fake->changeState(self::INSTANCE_ID, 'running');
     $this->fake->changeTerminable(self::INSTANCE_ID, true);
     $this->fake->changeStopAt(self::INSTANCE_ID, $one_hour_after);
-    $buttonText = 'stop_' . self::INSTANCE_ID;
-    $post_to = '/manual/stop/' . self::INSTANCE_ID . '/' . self::NICKNAME;
-    $this->visit('/')             //  ここにアクセスして
+    $this->visit('/')             //  ここにアクセスすると
+         ->see($one_hour_after)   //  これが表示されており、
          ->press('停止')          //  これを押したら
          ->seePageIs('/')         //  ここに戻って
          ->see('停止処理中')      //  これが表示されて
          ->dontSee($one_hour_after)
          ->see('手動モード');
+  }
+
+  /**
+   * 「起動」ボタンを押すと「起動処理中」に移行する
+   *
+   */
+  public function testEc2ManualsPressStart()
+  {
+    $one_hour_after = date('H:i:00', time() + 3600); //  現在時刻の１時間後
+    $this->fake->changeState(self::INSTANCE_ID, 'stopped');
+    $this->fake->changeTerminable(self::INSTANCE_ID, true);
+    $this->fake->changeStopAt(self::INSTANCE_ID, $one_hour_after);
+    $this->visit('/')                 //  ここにアクセスすると
+         ->dontSee($one_hour_after)   //  これは表示されず、
+         ->press('起動')              //  これを押したら
+         ->seePageIs('/')             //  ここに戻って
+         ->see('起動処理中')          //  これが表示されて
+         ->see('手動モード');         //  これが表示される
+  }
+
+  /**
+   * 「手動モードへ」ボタンを押すと「手動モード」に切り替わる
+   *
+   */
+  public function testEc2ManualsPressToManualMode()
+  {
+    $one_hour_after = date('H:i:00', time() + 3600); //  現在時刻の１時間後
+    $this->fake->changeState(self::INSTANCE_ID, 'running');
+    $this->fake->changeTerminable(self::INSTANCE_ID, true);
+    $this->fake->changeStopAt(self::INSTANCE_ID, $one_hour_after);
+    $this->visit('/')                 //  ここにアクセスすると
+         ->see($one_hour_after)       //  これが表示され、
+         ->press('手動モードへ')      //  これを押したら
+         ->seePageIs('/')             //  ここに戻って
+         ->dontSee($one_hour_after)   //  この表示が消えて
+         ->see('手動モード');         //  これが表示される
   }
 
 } //  class Ec2ManualsControllerTest extends TestCase
