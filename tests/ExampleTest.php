fa7ebd3f6bec31f122c3c971d649fa9459f5bf39
Michihide Hotta <hotta@net-newbie.com>
2016-11-10 11:52:01 +0900

    Add ExampleTest.php to complete test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * 基本的な機能テストの例
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->visit('/')
+             ->dontSee('Laravel 5');
+    }
+}
